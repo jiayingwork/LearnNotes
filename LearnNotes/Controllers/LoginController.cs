8dab9ff2520add804ada5a69596858ab3c605400
jiayingwork <943572120@qq.com>
2018-09-10 22:31:07 +0800

    登陆

---
+++ LearnNotes/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LearnNotes.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("api/login/savelogininfo/{loginname}/{password}")]
+        public HttpResponseMessage SaveLoginInfo(string LoginName ,string PassWord)
+        {
+
+            return Request.CreateResponse(HttpStatusCode.OK, LoginName + PassWord);
+        }
+    }
+}
